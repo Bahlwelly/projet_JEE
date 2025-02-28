fc6d19f5a8e247c715149dc4aab0d50060c62f61
Lwelly Bah <lwellyebah@gmail.com>
2025-02-28 23:55:19 +0000

    j'ai cree le class departement

---
+++ src/main/java/Classes/Departement.java
@@ -0,0 +1,47 @@
+package Classes;
+
+public class Departement {
+	private String id;
+	private String nom;
+	private Integer nombre_employees;
+	private String id_responsable;
+	
+	public  Departement( String id, String nom, Integer nombre_employee, String id_responsable ) {
+		this.id = id;
+		this.nom = nom;
+		this.nombre_employees = nombre_employee;
+		this.id_responsable = id_responsable;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNombre_employees(Integer nombre_employees) {
+		this.nombre_employees = nombre_employees;
+	}
+	
+	public Integer getNombre_employees() {
+		return nombre_employees;
+	}
+	
+	public void setId_responsable(String id_responsable) {
+		this.id_responsable = id_responsable;
+	}
+	
+	public String getId_responsable() {
+		return id_responsable;
+	}
+}
